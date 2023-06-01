e706332c488513bf44b1a0aa484ab09c001af30d
Zishan Ali <zishanali786.za@gmail.com>
2023-06-02 02:57:42 +0530

    Update 3

--- src/services/getServices.js
+++ src/services/getServices.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-const URL = 'https://birlingo.de:8081/api/webservice/'; // live
-// const URL = 'https://admin.birlingo.de:17276/api/webservice'; 
-// const URL = 'http://172.16.11.252:8081/api/webservice/';
-//  const URL = 'https://birlingo.devtechnosys.tech:17275/api/webservice/';
-// http://172.16.11.154:8081/api/webservice
+const URL = 'https://example.com/api/webservice/';
+
 export const getService = (urlAction, getParams) => {
   let ServiceUrl = URL + urlAction;
 
