bc09a3b4adba74be01efe3f3f95c0d193e14be4b
Zishan Ali <zishanali786.za@gmail.com>
2023-06-02 02:56:46 +0530

    Update 2

--- src/services/Constant.js
+++ src/services/Constant.js
@@ -2,12 +2,6 @@ export const API = {
 };
 
 export const BASEURL =
-  "https://birlingo.de:8081/public/webservice/";
-  // 'http://admin.birlingo.de:17276/api/webservice/'; 
-  // "https://birlingo.devtechnosys.tech:17275/api/webservice/";
-  // "http://172.16.11.252:8081/api/webservice/";
+  "https://example.com/api/webservice/";
 export const ImageBASEURL =
-  "https://birlingo.de:8081/public/uploads/user/";
-  // 'http://admin.birlingo.de:17276/api/webservice/'; 
-  // "https://birlingo.devtechnosys.tech:17275/api/webservice/";
-  // 'http://172.16.11.252:8081/api/webservice/';
+  "https://example.com/public/uploads/user/";
